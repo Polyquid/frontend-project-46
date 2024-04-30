918c08a2ac231dc761b77ad3ce3d7aad8c03e07d
Rakhimzhan <zhuba.sz@gmail.com>
2024-04-30 19:10:35 +0600

    fixed bugs

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -7,7 +7,7 @@ import parseFile from '../src/modules/parseFile.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const getFixturePath = (filename) => path.resolve(__dirname, '..', '__fixtures__', filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 test('genDiff, expect flat structure, format - "plain", read local file', () => {
   const expectFixturePath = getFixturePath('expectFlat.txt');
